137fa4872d15b454ee92687eb63b447048443042
your name <exam@mail.com>
2021-04-12 02:43:50 +0530

    Test cases for max string on three positions

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -1,17 +1,34 @@
 package com.bridgelabz;
-
 public class Maximum {
-
-    public Integer findmax(Integer firstPosition,Integer secondPosition,Integer thirdPosition){
-        Integer max=firsPosition;
+    public Integer findMaxInteger(Integer firstPosition,Integer secondPosition,Integer thirdPosition){
+        Integer max=firstPosition;
 
         if(secondPosition.compareTo(max)>0){
-            return secondPosition;
+            max=secondPosition;
         }
         if(thirdPosition.compareTo(max)>0){
-            return thirdPosition;
+            max=thirdPosition;
         }
-        else
         return max;
+    }
+    public Double findMaxDouble(Double firstPosition, Double secondPosition, Double thirdPosition) {
+        Double maxDouble = firstPosition;
+        if (secondPosition.compareTo(maxDouble) > 0) {
+            maxDouble = secondPosition;
+        }
+        if (thirdPosition.compareTo(maxDouble) > 0) {
+            maxDouble = thirdPosition;
+        }
+        return maxDouble;
+    }
+    public String findMaxString(String firsposition,String secondPosition, String thirdPosition){
+        String maxString=firsposition;
+        if(secondPosition.compareTo(maxString)>0){
+            maxString=secondPosition;
+        }
+        if(thirdPosition.compareTo(maxString)>0){
+            maxString=thirdPosition;
+        }
+        return maxString;
     }
 }
--- src/test/java/com/bridgelabz/MaximumTest.java
+++ src/test/java/com/bridgelabz/MaximumTest.java
@@ -2,48 +2,72 @@ package com.bridgelabz;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 public class MaximumTest {
-    @Test
-    public void givenMaxAtFirst_ShouldReturnSameNumber (){
-        Maximum maximum=new Maximum();
-        Integer highest=maximum.findmax(20,15,10);
-        Assert.assertEquals(20,(int)highest);
 
+    @Test
+    public void givenMaxAtFirst_ShouldReturnFirstNumber (){
+        Maximum maximum=new Maximum();
+        Integer highest=maximum.findMaxInteger(20,15,10);
+        Assert.assertEquals(20,(int)highest);
     }
+
     @Test
     public void givenMaxAtSecond_ShouldReturnSecondNumber (){
         Maximum maximum=new Maximum();
-        Integer highest=maximum.findmax(5,15,10);
         Integer highest=maximum.findMaxInteger(5,15,10);
         Assert.assertEquals(15,(int)highest);
-
     }
+
     @Test
     public void givenMaxAtThird_ShouldReturnThirdNumber (){
         Maximum maximum=new Maximum();
-        Integer highest=maximum.findmax(5,15,50);
         Integer highest=maximum.findMaxInteger(5,15,50);
         Assert.assertEquals(50,(int)highest);
-
     }
+
     @Test
     public void givenMaxDouble_AtFirst_shouldReturnFirstNumber(){
         Maximum maximum=new Maximum();
         Double highest=maximum.findMaxDouble(5.6,2.5,0.5);
         Assert.assertEquals((Double) 5.6,highest);
     }
+
     @Test
     public void givenMaxDouble_AtSecond_shouldReturnSecondNumber() {
         Maximum maximum = new Maximum();
         Double highest = maximum.findMaxDouble(43.45, 125.56, 65.78);
         Assert.assertEquals((Double) 125.56, highest);
     }
+
     @Test
     public void givenMaxDouble_AtThird_shouldReturnThirdNumber() {
         Maximum maximum = new Maximum();
         Double highest = maximum.findMaxDouble(0.1, 0.2, 0.3);
         Assert.assertEquals((Double) 0.3, highest);
     }
+
+    @Test
+    public void givenMaxString_AtFirstPosition_ShoulReturnFirstString(){
+        Maximum maximum=new Maximum();
+        String highest=maximum.findMaxString("Peach","Banana","Apple");
+        Assert.assertEquals((String) "Peach",highest);
+    }
+
+    @Test
+    public void givenMaxString_AtSecondPosition_ShoulReturnSecondString(){
+        Maximum maximum=new Maximum();
+        String highest=maximum.findMaxString("Java","Swift","Python");
+        Assert.assertEquals((String) "Swift",highest);
+    }
+
+    @Test
+    public void givenMaxString_AtThirdPosition_ShoulReturnThirdString(){
+        Maximum maximum=new Maximum();
+        String highest=maximum.findMaxString("Engineering","Commerce","Medicine");
+        Assert.assertEquals((String) "Medicine",highest);
+    }
+
 }
